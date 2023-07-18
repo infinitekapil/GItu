fee23d9fdaed50b03a86472e93f0febd89ec02d7
Kapil Yadav <hankap2000m@gmail.com>
2023-07-19 00:31:07 +0530

    first commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Sum {
+    public static void main(String args[]){
+        int a,b;
+        a=3;
+        b=5;
+        System.out.println("The sum is: " + a+b);
+    }
+}
